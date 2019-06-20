2dc8cf88b858baf735cdcdfeb92f479db159d919
Tyler Rick <tyler@tylerrick.com>
2019-06-20 10:40:42 -0700

    Output environment.toWebpackConfig() for debugging

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+console.log('environment.toWebpackConfig():')
+console.log(environment.toWebpackConfig())
 module.exports = environment.toWebpackConfig()
