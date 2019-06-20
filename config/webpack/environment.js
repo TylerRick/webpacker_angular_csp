328fdc4d408e562c98d7c6ecee70fe2410609b43
Tyler Rick <tyler@tylerrick.com>
2019-06-20 14:52:26 -0700

    Clean up
    - Move the angular loader code to config/webpack/loaders/angular.js
    - Remove the other typescript loader since angular loader handles typescript too
    - Remove unneeded config from tsconfig.json

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,10 @@
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-//const angular    =  require('./loaders/angular')
 const { AngularCompilerPlugin } = require('@ngtools/webpack')
+const angular         =  require('./loaders/angular')
 
-angular = {
-  //test: /(?:\.ngfactory\.js|\.ts\.ngfactory|\.ngstyle\.js|\.ts)$/,
-  test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
-  //test: /\.ts$/,
-  use: [
-    {
-      loader: '@ngtools/webpack'
-    }
-  ]
-}
+
 environment.loaders.prepend('angular', angular)
-//environment.loaders.prepend('typescript', typescript)
-
-environment.plugins.prepend(
-  'AngularCompiler',
+environment.plugins.prepend('angular',
   new AngularCompilerPlugin({
     tsConfigPath: './tsconfig.json',
     entryModule: './app/javascript/hello_angular/app/app.module.ts#AppModule',
--- config/webpack/loaders/angular.js
+++ config/webpack/loaders/angular.js
@@ -1,4 +1,5 @@
-const AngularCompilerPlugin = require('@ngtools/webpack')
+// Reference: https://www.npmjs.com/package/@ngtools/webpack#usage
+const { AngularCompilerPlugin } = require('@ngtools/webpack')
 
 module.exports = {
   test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
